7b354c23da78c5920a81c4bbc43480a13507ab34
shaoning <904809622@qq.com>
2021-03-11 13:03:11 +0800

    update for server error

--- PaperReader/wsgi.py
+++ PaperReader/wsgi.py
@@ -6,6 +6,8 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
+import sys
+sys.path.append('/home/ubuntu/my_pro/env_PaperReader/lib/python3.6/site-packages')  # 指向虚拟环境
 
 import os
 
